23dde20f38e526e838540f2fc5996914ca71879f
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-03-25 16:31:31 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,39 +7,38 @@ let userInput = prompt(" Guess The Game Number From 1 To 30 " + ( 10 - attempt) 
 
 while (userInput !== gameNum) {
     if(userInput === null || userInput.toLowerCase() === 'exit' || userInput.toUpperCase() === 'exit'){
-    alert("(You Exit The Game ) The Game Number Is " + gameNum  + ".");
-    break}
-
-    userInput = parseInt(userInput);
-    if (userInput === gameNum) {
-         alert("Correct! You win a watch that you will never get.");
+        alert("(You Exit The Game ) The Game Number Is " + gameNum  + ".");
+        break}
         
-        break;  
-
+        userInput = parseInt(userInput);
+        if (userInput === gameNum) {
+            //  alert("Correct! You win a watch that you will never get.");
+              
+            break;
+        }
+        if(isNaN(userInput)){
+            alert("You Entered An Invalid Number");
+        }else if (Math.abs(userInput - gameNum ) <= 1 ){
+            alert("You are Too Close To The Number");
+        }else if (Math.abs(userInput - gameNum ) <= 5){
+            alert("You Are somewhat Far Off To Guess The Number");
+        }else{
+            alert("You are too Far Off To Guess The Number");
+        }
+        attempt++;
+        
+        if (attempt === 9) {
+            alert("This Is Last Chance To Guess A Number");
+        }
+        if (attempt === 10) {
+            alert("You've used all your attempts. The correct number was " + gameNum + ".");
+            break;
+        }
+        
+        userInput = prompt("You Entered Wrong Number Keep Trying And Enter Exit Or Click Cancel To Quit " + ( 10 - attempt ) + " Guesses Left " );  
     }
-    if(isNaN(userInput)){
-        alert("You Entered An Invalid Number");
-    }else if (Math.abs(userInput - gameNum ) <= 1 ){
-        alert("You are Too Close To The Number");
-    }else if (Math.abs(userInput - gameNum ) <= 5){
-        alert("You Are somewhat Far Off To Guess The Number");
-    }else{
-        alert("You are too Far Off To Guess The Number");
-    }
-    attempt++;
-    userInput = prompt("You Entered Wrong Number Keep Trying And Enter Exit Or Click Cancel To Quit " + ( 10 - attempt ) + " Guesses Left " );  
+    
 
-     if (attempt === 9) {
-        alert("This Is Last Chance To Guess A Number");
-
-    if (userInput === gameNum) {
+    if (gameNum === userInput){
         alert("Correct! You win a watch that you will never get.");
-       
-       break}
-    if (attempt === 10) {
-        alert("You've used all your attempts. The correct number was " + gameNum + ".");
-        break;
     }
-   
-}
-}
